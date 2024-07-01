5947ca7765b26ad70f7b7f1cd9c211a838a4ace8
Anderson Gomes <anderson.stos.gomes@gmail.com>
2024-07-01 00:53:21 -0300

    feat: implementing smartphone models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
-﻿using DesafioPOO.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Samrtphone Nokia");
+Smartphone nokia = new Nokia(numero: "(11) 9761472-66", modelo: "Nokia 5200 Xpressmusic", imei: "11-000000X0003", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("");
+
+await Chamada(async () => { nokia.ReceberLigacao(); await Task.CompletedTask; });
+
+
+
+
+Console.WriteLine("");
+ await Task.Delay(5000);
+
+Console.WriteLine("Samrtphone Iphone");
+Smartphone iphone = new Iphone(numero: "(11) 9761472-66", modelo: "Iphone 15", imei: "11-000000X0003", memoria: 64);
+
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+Console.WriteLine("");
+
+await Chamada(async () => { iphone.ReceberLigacao(); await Task.CompletedTask; });
+
+
+
+
+
+
+
+
+
+
+
+static async Task Chamada(Func<Task> metodo)
+{
+    await Task.Delay(3000);
+    await metodo();
+}
